694f1c2624789d290d7e0311619ed4b8d92e35ba
Emmanuel Sambo <news@EmmanuelSambo.com>
2020-05-20 14:22:15 -0500

    Add more tests
    from:
    https://github.com/exercism/elixir/blob/master/exercises/bowling/test/bowling_test.exs
    
    Also change from :pending to :skip, as it works out of the box

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -4,6 +4,16 @@ defmodule Bowling do
   """
 
   @doc """
+  Groups a list of rolls (0-10) by frame.
+  Converts:
+  - 10 into "X" to indicate a strike
+  - 10 into "/" to indicate a spare
+  -  0 into "-" to indicate a miss
+  """
+  def rolls_to_frames(_rolls) do
+  end
+
+  @doc """
   Total score of the game at the end.
   """
   def score(_frames) do
